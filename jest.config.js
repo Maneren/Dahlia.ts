d725146f947128b6a30b2bb9e1c4561f5c224e63
Maneren <49210777+Maneren@users.noreply.github.com>
2023-03-16 22:52:04 +0100

    set up tooling

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
+  preset: "ts-jest",
+  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/[.].*"],
+  testEnvironment: "node",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  verbose: true,
 };
